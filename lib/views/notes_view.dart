4a8b703f17856ef6aef68370f6596e97d02de0dc
Ahlam Gomaa <ahlam.gomaa@example.com>
2024-08-10 19:52:35 +0300

    add border reduis to bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -12,6 +12,9 @@ class NotesView extends StatelessWidget {
         backgroundColor: Colors.blue.withOpacity(.5),
         onPressed: () {
           showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
               context: context,
               builder: (context) {
                 return const AddNoteSheet();
@@ -25,4 +28,3 @@ class NotesView extends StatelessWidget {
     );
   }
 }
-
