0c64af5eca35576f2617c51758201efa83693152
Ahlam Gomaa <ahlam.gomaa@example.com>
2024-08-11 18:49:59 +0300

    display notes part one

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -7,15 +7,15 @@ import 'package:notes_app/models/note_model.dart';
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
+
   NotesCubit() : super(NotesInitial());
+  List<NoteModel>? notes;
   fetchAllNotes() async {
  
-    try {
+  
       var notesBox = Hive.box<NoteModel>(kNotesBox);
-       List<NoteModel> notes=  notesBox.values.toList();
-      emit(NotesSuccess( notes));
-    } catch (e) {
-      emit(NotesFailure(e.toString()));
-    }
+      notes =  notesBox.values.toList();
+     
+  
   }
 }
