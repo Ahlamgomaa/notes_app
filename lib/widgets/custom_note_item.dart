573c7b053e346b86a23d9e1f62514af62214aff2
Ahlam Gomaa <ahlam.gomaa@example.com>
2024-08-11 19:48:08 +0300

    display data part two

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -21,16 +22,16 @@ class NoteItem extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
         decoration: BoxDecoration(
-          color: const Color(0xFFFFCC80),
+          color:  Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text(
-                'flutter tips',
-                style: TextStyle(
+              title: Text(
+                note.title,
+                style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     // fontFamily: 'Edu Australia VIC WA NT Hand',
                     color: Colors.black,
@@ -39,7 +40,7 @@ class NoteItem extends StatelessWidget {
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 20, bottom: 16),
                 child: Text(
-                  'Build your career with ahlam',
+                  note.subtitle,
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     // fontFamily: 'Edu Australia VIC WA NT Hand',
@@ -60,12 +61,13 @@ class NoteItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'may 5,2002',
+                note.date,
                 style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    // fontFamily: 'Edu Australia VIC WA NT Hand',
-                    color: Colors.black.withOpacity(.4),
-                    fontSize: 16),
+                  fontWeight: FontWeight.bold,
+                  // fontFamily: 'Edu Australia VIC WA NT Hand',
+                  color: Colors.black.withOpacity(.4),
+                  fontSize: 16,
+                ),
               ),
             ),
           ],
--- lib/widgets/custom_notes_list_view.dart
+++ lib/widgets/custom_notes_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_note_item.dart';
@@ -11,17 +12,19 @@ class NotesListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<NotesCubit, NotesState>(
       builder: (context, state) {
-
-        List<NoteModel> notes=BlocProvider.of<NotesCubit>(context).notes!;
+        List<NoteModel> notes =
+            BlocProvider.of<NotesCubit>(context).notes !;
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 16),
           child: ListView.builder(
-            itemCount:notes.length,
+              itemCount: notes.length,
               padding: EdgeInsets.zero,
               itemBuilder: (context, index) {
-                return const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 4),
-                  child: NoteItem(),
+                return Padding(
+                  padding:const EdgeInsets.symmetric(vertical: 4),
+                  child: NoteItem(
+                    note: notes[index],
+                  ),
                 );
               }),
         );
